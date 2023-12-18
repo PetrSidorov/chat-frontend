8bc837d636e4d99c5a6faa29f80b2b1286ef299e
Petr Sidorov <piterpatrikk@gmail.com>
2023-12-18 11:30:20 +0400

    figuring out websockets

---
+++ src/pages/Login.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+export default function Login() {
+  const [isNewUser, setIsNewUser] = useState(false);
+  const [formData, setFormData] = useState<{
+    username: string;
+    password: string;
+  }>({ username: "", password: "" });
+
+  function handleChange(e) {
+    setFormData((data) => {
+      return {
+        ...data,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+
+  function submitForm() {}
+  return (
+    <div className="w-full max-w-xs">
+      <form
+        onSubmit={submitForm}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none borderrounded w-full py-2 px-3
+            text-gray-700 leading-tight focus:outline-none
+            focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border border-red-500 rounded
+            w-full py-2 px-3 text-gray-700 mb-3
+            leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <p className="text-red-500 text-xs italic">
+            Please choose a password.
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold
+            py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign In
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm
+            text-blue-500 hover:text-blue-800"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+      </form>
+      <p className="text-center text-gray-500 text-xs">
+        &copy;2020 Acme Corp. All rights reserved.
+      </p>
+    </div>
+  );
+}
