f5361265c4bbb73ba953d2f5e1e9e50c59443add
Petr Sidorov <piterpatrikk@gmail.com>
2024-03-03 15:48:25 +0400

    redirection fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AuthGuard from "./components/AuthGuard";
+import ActiveConvoProvider from "./context/AllConvoContext";
+import AuthProvider from "./context/AuthProvider";
+import ResizeProvider from "./context/ResizeProvider";
+import ChatMain from "./pages/Chat/ChatMain";
 import Login from "./pages/Login/Login";
 import StartScreen from "./pages/StartScreen/StartScreen";
-import ChatMain from "./pages/Chat/ChatMain";
-import AuthProvider from "./context/AuthProvider";
-import AvatarProvider from "./context/AvatarProvider";
-import ResizeProvider from "./context/ResizeProvider";
-import ActiveConvoProvider from "./context/AllConvoContext";
-import AuthGuard from "./components/AuthGuard";
-// import NavigationProvider from "./context/NavigationContext";
 
 function App() {
   return (
@@ -22,6 +20,7 @@ function App() {
                 <Route path="/settings" element={<ChatMain />}></Route>
                 <Route path="/friends" element={<ChatMain />}></Route>
                 <Route path="/messages" element={<ChatMain />}></Route>
+                <Route path="*" element={<Navigate to="/login" replace />} />
               </Route>
             </Routes>
           </ResizeProvider>
